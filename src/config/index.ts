3e363c21ca6ec037ed50e03c410e06588682f7ee
家中台式电脑 <1624923037@qq.com>
2024-07-04 01:35:04 +0800

    新增启动页样式

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+// export * from "./modules/scene-tip";
+// export * from "./modules/route-path";
+// export * from "./modules/resource-name";
+// export * from "./modules/mouse-tip";
+// export * from "./modules/message-tip";
+// export * from "./modules/local-key";
+// export * from "./modules/help-tip";
+// export * from "./modules/confirm-tip";
+export * from "./modules/base";
+// export * as YIBAO_PART from "./modules/yibao-part";
+// export * as MARKER_CONFIG from "./modules/marker-config";
+// export * as GAME_PROP from "./modules/game-prop";
+// export * as GAME_CONFIG from "./modules/game-config";
+// export * as GAME_CHANCE from "./modules/game-chance";
+// export * as DEFAULT from "./modules/default";
---
+++ src/config/modules/base.ts
@@ -0,0 +1,7 @@
+/** @description 基础配置 */
+export const BASE_CONFIG = {
+	/** 首页地址 */
+	HOME_URL: "/hero",
+	/** 图床链接 */
+	IMGBED: "https://lengyibai.github.io/wzry-material"
+};
